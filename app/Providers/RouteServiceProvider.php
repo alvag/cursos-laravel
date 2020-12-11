02e4c0831000ee21e3f3bcb273ba58a72197c095
Max Alva <malva@peruapps.com.pe>
2020-12-10 19:54:20 -0500

    install adminLTE

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,8 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/dashboard';
+//    public const HOME = '/dashboard';
+    public const HOME = '/';
 
     /**
      * The controller namespace for the application.
@@ -37,16 +38,21 @@ class RouteServiceProvider extends ServiceProvider
     {
         $this->configureRateLimiting();
 
-        $this->routes(function () {
-            Route::prefix('api')
-                ->middleware('api')
-                ->namespace($this->namespace)
-                ->group(base_path('routes/api.php'));
+        $this->routes( function () {
+            Route::prefix( 'api' )
+                ->middleware( 'api' )
+                ->namespace( $this->namespace )
+                ->group( base_path( 'routes/api.php' ) );
 
-            Route::middleware('web')
-                ->namespace($this->namespace)
-                ->group(base_path('routes/web.php'));
-        });
+            Route::middleware( 'web' )
+                ->namespace( $this->namespace )
+                ->group( base_path( 'routes/web.php' ) );
+
+            Route::middleware( [ 'web', 'auth' ] )
+                ->namespace( $this->namespace )
+                ->prefix( 'admin' )
+                ->group( base_path( 'routes/admin.php' ) );
+        } );
     }
 
     /**
@@ -56,8 +62,8 @@ class RouteServiceProvider extends ServiceProvider
      */
     protected function configureRateLimiting()
     {
-        RateLimiter::for('api', function (Request $request) {
-            return Limit::perMinute(60)->by(optional($request->user())->id ?: $request->ip());
-        });
+        RateLimiter::for( 'api', function ( Request $request ) {
+            return Limit::perMinute( 60 )->by( optional( $request->user() )->id ? : $request->ip() );
+        } );
     }
 }
