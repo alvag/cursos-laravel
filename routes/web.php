06e4acf126baba93bd4edf90e9fb51b9153d6ca3
Max Alva <malva@peruapps.com.pe>
2020-12-10 20:26:52 -0500

    configuracion de plantilla

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get( '/', function () {
+    return view( 'welcome' );
+} )->name( 'home' );
 
-Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
-    return view('dashboard');
-})->name('dashboard');
+Route::middleware( [ 'auth:sanctum', 'verified' ] )->get( '/dashboard', function () {
+    return view( 'dashboard' );
+} )->name( 'dashboard' );
