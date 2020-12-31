6815f52d10f199f294dee1d3154e765b9b1a9b9b
Max Alva <malva@peruapps.com.pe>
2020-12-31 10:42:50 -0500

    Listado de cursos de un instructor

--- app/Http/Livewire/InstructorCourse.php
+++ app/Http/Livewire/InstructorCourse.php
@@ -2,12 +2,27 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Course;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class InstructorCourse extends Component
 {
+    use WithPagination;
+
+    public $search;
+
     public function render()
     {
-        return view('livewire.instructor-course');
+        $courses = Course::where( 'user_id', auth()->user()->id )
+            ->where( 'title', 'LIKE', '%' . $this->search . '%' )
+            ->paginate( 8 );
+
+        return view( 'livewire.instructor-course', compact( 'courses' ) );
+    }
+
+    public function clearPage()
+    {
+        $this->reset( 'page' );
     }
 }
